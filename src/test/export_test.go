fc017c194a259d3e37c92dda71c8aa8362bcab44
zhuharev <hostmaster@zhuharev.ru>
2017-03-10 09:27:04 +0300

    update export tests

--- src/test/export_test.go
+++ src/test/export_test.go
@@ -11,6 +11,7 @@ func TestExport(t *testing.T) {
 	db, err := bolt.Open("/home/god/go/src/github.com/inpime/fader/_app.db", FilesPermission, &bolt.Options{
 		Timeout: 1 * time.Second,
 	})
+	defer db.Close()
 	assert.Nil(t, err)
 	assert.NotNil(t, db)
 
