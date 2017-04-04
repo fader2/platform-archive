96069ed6c1d3ebbe73474744e9905a049bc933da
zhuharev <hostmaster@zhuharev.ru>
2017-04-04 07:06:46 +0300

    add new method to api

--- src/api/addons.go
+++ src/api/addons.go
@@ -11,3 +11,11 @@ func SetupAddons() error {
 
 	return nil
 }
+
+func AddonList() (map[string]string, error) {
+	res := make(map[string]string)
+	for _, addon := range addons.Addons {
+		res[addon.Name()] = addon.Version()
+	}
+	return res, nil
+}
