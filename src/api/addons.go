ea1d233f12343303417719b45ee10473442f2034
zhuharev <hostmaster@zhuharev.ru>
2017-04-05 03:48:08 +0300

    add addons version checker

--- src/api/addons.go
+++ src/api/addons.go
@@ -3,6 +3,10 @@ package api
 import (
 	"addons"
 	"addons/example"
+	//"fmt"
+	"github.com/BurntSushi/toml"
+	//"github.com/hashicorp/go-version"
+	"io"
 )
 
 func SetupAddons() error {
@@ -18,4 +22,30 @@ func AddonList() (map[string]string, error) {
 		res[addon.Name()] = addon.Version()
 	}
 	return res, nil
+}
+
+type addonsSetting struct {
+	Addons map[string]string `toml:"addons"`
+}
+
+func CheckCompatibility(fpath string) (bool, error) {
+	var as addonsSetting
+	_, err := toml.DecodeFile(fpath, &as)
+	if err != nil {
+		return false, err
+	}
+
+	//for _, v := range as.Addons {
+	//	version.NewConstraint(fmt.Sprintf(">= %d, < %d"))
+	//}
+	return false, nil
+}
+
+func CheckCompatibilityFromReader(rdr io.Reader) (bool, error) {
+	var as addonsSetting
+	_, err := toml.DecodeReader(rdr, &as)
+	if err != nil {
+		return false, err
+	}
+	return false, nil
 }
